1cca94339f894969c9acde5d136aed2fa5aa4e61
OndraZizka <zizka@seznam.cz>
2012-11-13 14:31:13 +0100

    WIP

--- src/main/java/cz/oz/web/WicketJavaEEApplication.java
+++ src/main/java/cz/oz/web/WicketJavaEEApplication.java
@@ -2,15 +2,15 @@ package cz.oz.web;
 
 import cz.oz.web.pages.JTexyPage;
 import static net.ftlines.wicket.cdi.ConversationPropagation.NONE;
-
 import javax.enterprise.inject.spi.BeanManager;
+import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-
 import net.ftlines.wicket.cdi.CdiConfiguration;
-
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -18,15 +18,22 @@ import org.apache.wicket.protocol.http.WebApplication;
  * @author Ondrej Zizka
  */
 public class WicketJavaEEApplication extends WebApplication {
+    private static final Logger log = LoggerFactory.getLogger(WicketJavaEEApplication.class);
+    
+    private Settings settings = createSettings();
 
+    
     @Override
     public Class<? extends Page> getHomePage() {
         return JTexyPage.class;
     }
 
+    
     @Override
     protected void init() {
         super.init();
+        
+        log.info("Log Test");
 
         // Enable CDI
         BeanManager bm;
@@ -45,5 +52,30 @@ public class WicketJavaEEApplication extends WebApplication {
         // Mount all paths to JTexy.
         mountPage("/pages", JTexyPage.class);
     }
+
     
-}
+    
+    public Settings getSettings() {
+        return settings;
+    }
+
+    
+    private Settings createSettings() {
+        Settings settings = new Settings();
+        
+        // Get the value from web.xml's <env-entry>.
+        //settings.texyFilesRootPath = this.getServletContext().getResourceAsStream("texyFilesRootPath").toString();
+        try {
+            Context env = (Context)new InitialContext().lookup("java:comp/env");
+            settings.texyFilesRootPath = (String)env.lookup("texyFilesRootPath");
+        } catch( NamingException ex ){
+            // TODO
+        }
+        
+        return settings;
+    }
+    
+}// class
+class Settings {
+    String texyFilesRootPath;
+}// class
