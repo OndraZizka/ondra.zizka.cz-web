47bdecc39b5994b5ae8640032bfb8e59edb307b9
OndraZizka <zizka@seznam.cz>
2012-12-16 20:12:53 +0100

    Moved texy content to TexyDocumentPanel.

---
+++ src/main/java/cz/oz/web/_co/TexyDocumentPanel.java
@@ -0,0 +1,73 @@
+package cz.oz.web._co;
+
+import cz.dynawest.jtexy.JTexy;
+import cz.dynawest.jtexy.TexyException;
+import cz.oz.web.WicketJavaEEApplication;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+
+/**
+ *
+ *  @author Ondrej Zizka
+ */
+public class TexyDocumentPanel extends Panel {
+
+    private transient Charset encoding = Charset.forName("utf-8");
+
+    
+    public TexyDocumentPanel( String id, String reqPath ) {
+        super( id, new Model<String>(reqPath) );
+
+        // Filesystem path.
+        File rootPath = new File(((WicketJavaEEApplication)this.getApplication()).getSettings().getTexyFilesRootPath());
+        File texyFile = new File(rootPath, reqPath);
+
+        // Exists?
+        if( ! texyFile.exists() ){
+            add( new Label("substituteTitle", "Page not found."));
+            add( new Label("body", "Doesn't exist: " + texyFile.getPath() ));
+        }
+        else {
+            // Read.
+            String src;
+            try {
+                src = FileUtils.readFileToString(texyFile, this.encoding);
+            } catch (IOException ex) {
+                add( new Label("body", "Error reading " + texyFile.getPath() + ": " + ex.toString() ));
+                // TODO log
+                return;
+            }
+
+            // Convert.
+            String html;
+            try {
+                html = JTexy.create().process(src);
+            } catch( TexyException ex ){
+                add( new Label("body", "Error rendering " + texyFile.getPath() + ": " + ex.toString() ));
+                // TODO log
+                return;
+            }
+
+            // Show.
+            add( new Label("body", html).setEscapeModelStrings(false));
+
+            // Add title if absent in document.
+            add( new Label("substituteTitle", guessTitle(texyFile.getName())).setVisibilityAllowed(false) );
+        }
+    }// const
+
+
+    /**
+     *  foo-bar-baz.texy --> Foo bar baz.
+     */
+    public static String guessTitle( String name ) {
+        return StringUtils.capitalize( name ).replaceAll("-", " ");
+    }
+
+}
--- src/main/java/cz/oz/web/_pg/JTexyPage.java
+++ src/main/java/cz/oz/web/_pg/JTexyPage.java
@@ -1,18 +1,12 @@
 package cz.oz.web._pg;
 
-import cz.dynawest.jtexy.JTexy;
-import cz.dynawest.jtexy.TexyException;
 import cz.oz.web.WicketJavaEEApplication;
+import cz.oz.web._co.TexyDocumentPanel;
 import cz.oz.web._co.baseLayout.BaseLayoutPage;
 import cz.oz.web.dao.TexyFileDaoBean;
 import java.io.File;
-import java.io.IOException;
 import java.nio.charset.Charset;
 import javax.inject.Inject;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
-
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 
@@ -29,17 +23,12 @@ public class JTexyPage extends BaseLayoutPage implements ICountablePage {
 
     @Inject private transient TexyFileDaoBean dao;
 
-
-    private transient Charset encoding = Charset.forName("utf-8");
-
     // ID of the page to show.
     private String reqPath;
-
 
     
     // Set up the dynamic behavior for the page, widgets bound by id
     public JTexyPage(PageParameters params) {
-
         
         // Construct the path to the texy file.
         StringBuilder sb = new StringBuilder();
@@ -48,49 +37,8 @@ public class JTexyPage extends BaseLayoutPage implements ICountablePage {
         }
         // Requested path. May be served from cache/db.
         this.reqPath = sb.toString();
-        
-        // Filesystem path.
-        File rootPath = new File(((WicketJavaEEApplication)this.getApplication()).getSettings().getTexyFilesRootPath());
-        File texyFile = new File(rootPath, reqPath);
 
-        if( ! texyFile.exists() ){
-            add( new Label("substituteTitle", "Page not found."));
-            add( new Label("content", "Doesn't exist: " + texyFile.getPath() ));
-        }
-        else {
-            // Read.
-            String src;
-            try {
-                src = FileUtils.readFileToString(texyFile, this.encoding);
-            } catch (IOException ex) {
-                add( new Label("content", "Error reading " + texyFile.getPath() + ": " + ex.toString() ));
-                // TODO log
-                return;
-            }
-            
-            // Convert.
-            String html;
-            try {
-                html = JTexy.create().process(src);
-            } catch( TexyException ex ){
-                add( new Label("content", "Error rendering " + texyFile.getPath() + ": " + ex.toString() ));
-                // TODO log
-                return;
-            }
-            
-            // Show.
-            add( new Label("content", html).setEscapeModelStrings(false));
-
-            // Add title if absent in document.
-            add( new Label("substituteTitle", guessTitle(texyFile.getName())).setVisibilityAllowed(false) );
-        }
-    }
-
-    /**
-     *  foo-bar-baz.texy --> Foo bar baz.
-     */
-    private static String guessTitle( String name ) {
-        return StringUtils.capitalize( name ).replaceAll("-", " ");
+        add( new TexyDocumentPanel("document", reqPath) );
     }
 
 
